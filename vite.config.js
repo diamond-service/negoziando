0436bcc9c4732c4a5f3d9b0d91bd1ba5e51f1181
diamond-service <diamond.service.livorno@gmail.com>
2025-04-25 23:06:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['recharts']
-    }
-  }
+  plugins: [react()]
+  // Rimuovi la configurazione di rollupOptions che imposta recharts come esterno
 });
