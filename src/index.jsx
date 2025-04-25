b8cdd8788101c5058d47d20b75199073fca504d2
diamond-service <diamond.service.livorno@gmail.com>
2025-04-25 19:55:26 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // Se vuoi aggiungere un file di stile globale (opzionale)
+import React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
