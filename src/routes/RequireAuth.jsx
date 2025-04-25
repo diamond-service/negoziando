06eba8809d2f105e79f6cf8ea2507b855f9c64c9
diamond-service <diamond.service.livorno@gmail.com>
2025-04-25 19:58:02 +0200

    Update RequireAuth.jsx

--- src/routes/RequireAuth.jsx
+++ src/routes/RequireAuth.jsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
+import React from "react";
 
 export function RequireAuth({ children }) {
   const { user } = useAuth();
