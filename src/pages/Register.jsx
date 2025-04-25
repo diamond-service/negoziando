5fad49740b189b1381001e29d7623ce896e3984a
diamond-service <diamond.service.livorno@gmail.com>
2025-04-25 19:56:37 +0200

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabaseClient';
+import React from "react";
 
 export default function Register() {
   const { register } = useAuth();
