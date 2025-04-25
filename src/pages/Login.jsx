c9c6c1e77f16f0aea79538f8a5c8844e1953a632
diamond-service <diamond.service.livorno@gmail.com>
2025-04-25 19:56:22 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import React from "react";
 
 export default function Login() {
   const { login } = useAuth();
