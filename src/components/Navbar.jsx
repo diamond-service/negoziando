058d49ddc0217ab909dbad44223e6c3ddfcb2270
diamond-service <diamond.service.livorno@gmail.com>
2025-04-26 15:20:49 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,6 +55,7 @@ export default function Navbar() {
                 >
                   Dashboard Venditore
                 </Link>
+
               )}
               {role === "admin" && (
                 <Link
